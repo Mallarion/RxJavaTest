360a3c5837c785d46a59f291cafc7f2b882c8404
nikolay <briedis230314@gmail.com>
2022-11-06 17:55:23 +0300

    feat: initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        List<String>  arrayList = new ArrayList<>();
+        Collections.addAll(arrayList, "s", "fg", "df", "nik");
+
+
+
+
+//String result;
+
+//        Observable<String> observable = Observable.just("Hello, Mike")
+//                .map(s->s +" OMG")
+//                .map(i -> i.replace("Mike", "John"))
+//                .map(u -> u + " HEY");
+//
+//        observable.subscribe();
+//        Thread.sleep(1500);
+//        observable.subscribe();
+
+
+
+
+//        startFunc("Ben", "John");
+    }
+
+//    public static void startFunc(String ... args){
+//        Flowable.fromArray(args).subscribe(s-> System.out.println("Hello "+ s+ "!"));
+//    }
+    public int testSynchronized(int testA) throws InterruptedException {
+        Thread.sleep(1500);
+        System.out.println("we in method!");
+        return ++testA;
+    }
+}
---
+++ src/main/java/org/example/observable/ObservableUsingJust.java
@@ -0,0 +1,4 @@
+package org.example.observable;
+
+public class ObservableUsingJust {
+}
